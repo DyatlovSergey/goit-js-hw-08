3b620c2c87bba935a3c7b7ac7aec4ab4c4ca5233
DyatlovSergey <s.dyatlov90@gmail.com>
2021-11-19 23:48:16 +0200

    1 input

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+populateTextarea();
+function onFormSubmit(evt) {
+  evt.preventDefault();
 
+  console.log('Отправляем форму');
+
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-msg');
+}
+
+function onTextareaInput(evt) {
+  const message = evt.currentTarget.value;
+  localStorage.setItem('feedback-msg', message);
+  //   console.log(localStorage.getItem('feedback-msg'));
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-msg');
+
+  if (savedMessage) {
+    console.log(savedMessage);
+    refs.textarea.value = savedMessage;
+  }
+}
